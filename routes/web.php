076dd0ca304ce28056e8d968176828b91edbbe24
Umer Inayat <umerinayat.369@gmail.com>
2020-07-14 03:37:58 +0500

    Email verification removed

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     $user = Auth::user();
@@ -22,6 +13,6 @@ Route::get('/', function () {
 
 });
 
-Auth::routes(['verify' => true]);
-Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
-Route::get('/{any?}', 'HomeController@index')->where( 'any', '.*' )->middleware('verified');
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{any?}', 'HomeController@index')->where( 'any', '.*' );
